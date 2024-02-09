d8f4e63f6915ee71b2b7ca24f71abbb984e65c7c
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-02-09 19:18:38 -0300

    vendo modal

--- to-do-list-app/src/app/view/modal-criar/modal-criar.component.ts
+++ to-do-list-app/src/app/view/modal-criar/modal-criar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
 
 @Component({
   selector: 'app-modal-criar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './modal-criar.component.html',
-  styleUrl: './modal-criar.component.css'
+  styleUrl: './modal-criar.component.css',
 })
 export class ModalCriarComponent {
-
 }
